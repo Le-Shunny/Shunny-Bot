d6ef7b2149248877546f240fe4bce494b70c06af
PoPuRo43 <carandangzane0@gmail.com>
2023-12-15 00:01:23 +0800

    letsgo

---
+++ modules/commands/mania-osu.js
@@ -0,0 +1,22 @@
+module.exports.config = {
+  name: "osumania",
+  version: "1.0.3",
+  hasPermssion: 0,
+  credits: "Shen",
+  description: "Get osumania info through username",
+  commandCategory: "game",
+  usages: "[username]",
+  cooldowns: 5,
+  dependencies: {
+    "request": "",
+    "fs-extra": "",
+    "axios": ""
+  }
+};
+
+
+module.exports.run = ({ event, api, args, getText }) => {
+    if (args.length == 0) return api.sendMessage("please provide a valid username!", event.threadID, event.messageID);
+    const request = global.nodemodule["request"];
+    const fs = global.nodemodule["fs-extra"];
+    request(`https://lemmmy.pw/osusig/sig.php?colour=pink&uname=${args.join(" ")}&mode=3&countryrank&onlineindicator=undefined`).pipe(fs.createWriteStream(__dirname + `/cache/${event.senderID}-osu.png`)).on("close", () => api.sendMessage({ attachment: fs.createReadStream(__dirname + `/cache/${event.senderID}-osu.png`) }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/${event.senderID}-osu.png`), event.messageID));}
